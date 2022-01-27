479e25f41f5b275213b268b2e69816f55337f314
Randolph Cameron Rozean <rcrozean@gmail.com>
2022-01-27 12:30:48 -0600

    Added skeleton structure for website

---
+++ pages/about.tsx
@@ -0,0 +1,5 @@
+import Conatiner from 'components/Container';
+
+export default function About() {
+  return <Conatiner>About</Conatiner>;
+}
---
+++ pages/guestbook.tsx
@@ -0,0 +1,5 @@
+import Container from 'components/Container';
+
+export default function Guestbook() {
+  return <Container>Guestbook</Container>;
+}
