f9fa4891ba38d352a3dd33eb1bba649f9c90afae
Randolph Cameron Rozean <rcrozean@gmail.com>
2022-04-26 17:03:30 -0500

    updated uniform formating amoung h1 tags, and wording for About

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,18 +10,28 @@ export default function Home() {
   return (
     <Container>
       <div className="mx-auto mb-8 w-full max-w-4xl flex-col items-start justify-center pb-8">
-        <h1 className="text-3xl">Randolph "Cameron" Rozean</h1>
+        <h1 className="text-3xl font-bold">Randolph "Cameron" Rozean</h1>
         <h2 className="text-sm">
           Full Stack Software Engineer at{' '}
           <span className="font-bold">Schlumberger</span>
         </h2>
         <hr />
         <div className="pb-4">
-          <h1 className="text-2xl">About</h1>
+          <h1 className="text-2xl font-semibold">About</h1>
           <p>
-            Howdy! My name is Cameron Rozean. I need to figure out what else to
-            put here. I hate this part of writing...
+            Howdy! My name is Cameron Rozean. I am a full stack software
+            engineer with a passion for technology. I have experience in IT
+            helpdesk, system administration, automation, and most recently
+            software engineering. Currently working on a large ML/Robotics
+            project, a GIS mapping project, and a few other personal (for fun)
+            Web Development projects, that as I clean up will begin sharing in
+            the{' '}
+            <a className="font-semibold" href="#projects">
+              Projects
+            </a>{' '}
+            section. Check back in for more updates.
           </p>
+          <br />
           <p>Here are a few technologies I have been working with:</p>
           <div className="grid grid-cols-5">
             <ul className="col-start-2 list-inside list-disc">
@@ -37,14 +47,14 @@ export default function Home() {
           </div>
         </div>
         <div className="pb-4">
-          <h1 className="text-2xl">GitHub Projects</h1>
+          <h1 className="text-2xl font-semibold" id="projects">
+            Projects
+          </h1>
         </div>
-        <div className="hidden pb-4">
-          <h1>Featured Blog Posts</h1>
-          <BlogPost />
-        </div>
-        <div className="hidden pb-4">
-          <h1>Recent Blog Posts</h1>
+        <div className="pb-4">
+          <h1 className="text-2xl font-semibold" id="blogposts">
+            Blog Posts
+          </h1>
           <BlogPost />
         </div>
       </div>
